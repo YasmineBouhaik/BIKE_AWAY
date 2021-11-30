47e32d5f95b45848bdbb76cf13dd47e5f31964ab
YasmineBouhaik <yasmine.bouhaik@gmail.com>
2021-11-30 17:46:28 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "profile", to: "pages#profile"
+
+  resources :rides, only: [:index, :show] do
+    resources :participants, only: :create
+    resources :messages, only: :create
+  end
+
+  resources :itineraries do
+    resources :rides, only: :create
+  end
+
+  resources :steps, only: :destroy
 end
