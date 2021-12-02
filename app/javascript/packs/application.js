204bd6622593c631f87725addc9805ee1a75ba57
marwa homsi <marwa.9101@hotmail.com>
2021-12-02 17:28:00 +0100

    key map not public

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import mapboxgl from 'mapbox-gl';
+
 
 Rails.start()
 Turbolinks.start()
@@ -24,17 +24,14 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+  // map
+  import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
-  // map
- 
 
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [ -0.077, 51.533 ],
-    zoom: 12
-  });
+
+
+  
