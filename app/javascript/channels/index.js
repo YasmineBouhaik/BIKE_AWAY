b20ee9b91430715877fa50d494eb3321eb9978a7
marwa homsi <marwa.9101@hotmail.com>
2021-12-03 11:21:39 +0100

    display chat div on click

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,9 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
+import { displayChat } from '../components/choose_ride_btn';
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const btn = document.querySelector('.btn-choose-ride');
+btn.addEventListener('click', displayChat);
