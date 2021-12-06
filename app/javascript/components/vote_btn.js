3a10abe608fc47ebdb67773dd2fb27c586675022
marwa homsi <marwa.9101@hotmail.com>
2021-12-06 22:20:28 +0100

    debug display chat div, onclick on the btn vote change color

--- app/javascript/components/vote_btn.js
+++ app/javascript/components/vote_btn.js
@@ -1,18 +1,22 @@
 const changeHeartColor = (event) => {
   event.preventDefault();
     const heartIcon = document.querySelector(".heart-icon");
-    heartIcon.style.color = "#FF4C4C";
+    if (heartIcon) {
+      heartIcon.style.color = "#FF4C4C";
+    }
 }
 
-const addAVote = (event) => {
-  event.preventDefault();
-  const votes = document.querySelector(".votes");
-  votes =+ 1;
-}
+// const addAVote = (event) => {
+//   event.preventDefault();
+//   const votes = document.querySelector(".votes");
+//   votes =+ 1;
+// }
 
 const heartClick = () => {
   const btnHeart = document.querySelector(".btn-heart");
-  btnHeart.addEventListener('click', changeHeartColor);
-  btnHeart.addEventListener('click', addAVote);
+  if (btnHeart) {
+    btnHeart.addEventListener('click', changeHeartColor);
+    btnHeart.addEventListener('click', addAVote);
+  }
 }
 export { heartClick };
