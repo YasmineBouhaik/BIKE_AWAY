32316cd81cbe02d5953fd1180631be8e54b0c436
marwa homsi <marwa.9101@hotmail.com>
2021-12-09 18:00:12 +0100

    chat style

--- app/javascript/components/choose_ride_btn.js
+++ app/javascript/components/choose_ride_btn.js
@@ -1,15 +1,22 @@
+import { event } from "jquery";
+
 const displayChat = (event) => {
   event.preventDefault();
   const chat = document.querySelector(".chat");
   if (chat){
-    
     chat.style.display = "block";
   }
 }
 
+// const alert = (event) => {
+ 
+//   console.log('helloooooo alert');
+// }
+
 const btnClick = () => {
   const btn = document.querySelector('.btn-choose-ride');
   if (btn) {
+  
     btn.addEventListener('click', displayChat);
   }
 }
